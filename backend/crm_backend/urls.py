57f99f97c05641a7af8532635f7b6be76ca39b20
Vraj Patel <mycsstuff@outlook.com>
2025-09-17 15:39:43 -0400

    Backend: build the API endpoints.

--- backend/crm_backend/urls.py
+++ backend/crm_backend/urls.py
@@ -14,9 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from students.views import (
+    StudentViewSet,
+    StudentProfileViewSet,
+    InteractionViewSet,
+    InternalNoteViewSet,
+)
+
+# Create a router to automatically generate URL patterns for our ViewSets
+router = DefaultRouter()
+router.register(r"students", StudentViewSet)
+router.register(
+    r"student-profiles", StudentProfileViewSet
+)  # We'll use this for the individual profile view
+router.register(r"interactions", InteractionViewSet)
+router.register(r"notes", InternalNoteViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "api/", include(router.urls)
+    ),  # Include the API URLs under the 'api/' namespace
 ]
